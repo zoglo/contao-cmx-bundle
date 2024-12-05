7ae6367fc7aec815c39ad421d7c43c7b82ddc9c1
Kate <kzimmermann@kiwi.de>
2024-12-05 14:21:18 +0100

    fix: Use correct namespace

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,6 +6,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Kiwi\Contao\CmxBundle\KiwiCmxBundle;
 
 class Plugin implements BundlePluginInterface
 {
