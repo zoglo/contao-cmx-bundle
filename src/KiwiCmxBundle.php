8a58aa88fef8398e6592a93c00f817577809e135
Kate <kzimmermann@kiwi.de>
2024-12-10 19:08:16 +0100

    extend abstract bundle in bundle class

--- src/KiwiCmxBundle.php
+++ src/KiwiCmxBundle.php
@@ -2,12 +2,8 @@
 
 namespace Kiwi\Contao\CmxBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class KiwiCmxBundle extends Bundle
+class KiwiCmxBundle extends AbstractBundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
 }
