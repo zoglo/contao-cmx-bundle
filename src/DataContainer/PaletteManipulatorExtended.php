cd44d5d1dc6f8d5e17bccdadd0e18ebb7ef914e5
Kate <kzimmermann@kiwi.de>
2024-12-10 11:51:24 +0100

    add functionto apply to multiple palettes

--- src/DataContainer/PaletteManipulatorExtended.php
+++ src/DataContainer/PaletteManipulatorExtended.php
@@ -4,19 +4,30 @@ namespace Kiwi\Contao\CmxBundle\DataContainer;
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-class PaletteManipulatorExtended extends PaletteManipulator{
+class PaletteManipulatorExtended extends PaletteManipulator
+{
     public static function create(): self
     {
         return new self();
     }
 
-    public function applyToAllPalettes(string $table, array $arrExceptions = []):object
+    public function applyToAllPalettes(string $table, array $arrExceptions = []): object
     {
         foreach ($GLOBALS['TL_DCA'][$table]['palettes'] as $strPalette => $varFields) {
-            if(!is_string($varFields) || in_array($strPalette, ($arrExceptions ?? []))) continue;
+            if (!is_string($varFields) || in_array($strPalette, ($arrExceptions ?? []))) continue;
 
             $this->applyToPalette($strPalette, $table);
         }
+        return $this;
+    }
+
+    public function applyToPalettes(array $names, string $table): self
+    {
+        foreach ($names as $name) {
+            if(!($GLOBALS['TL_DCA'][$table]['palettes'][$name] ?? false)) continue;
+            parent::applyToPalette($name, $table);
+        }
+
         return $this;
     }
 }
