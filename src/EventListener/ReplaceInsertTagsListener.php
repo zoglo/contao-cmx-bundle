8b12f2a6a73c79f438680a9712746da0f446c98d
Kate <kzimmermann@kiwi.de>
2025-08-15 16:03:45 +0200

    Add anchor inserttag and picker view

---
+++ src/EventListener/ReplaceInsertTagsListener.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Kiwi\Contao\CmxBundle\EventListener;
+
+use Contao\ArticleModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Routing\Content\PageResolver;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Contao\PageModel;
+use Contao\StringUtil;
+use Contao\System;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+#[AsHook("replaceInsertTags")]
+class ReplaceInsertTagsListener
+{
+    public function __construct(protected ContentUrlGenerator $pageResolverService)
+    {
+    }
+
+    public function __invoke(string $strTag)
+    {
+        if ($strTag === 'nbsp') {
+            return '&nbsp;';
+        } elseif ($strTag === 'shy') {
+            return '&shy;';
+        }
+
+        $arrTag = explode('::', $strTag);
+
+        if ($arrTag[0] == 'anchor_url') {
+            $objPage = System::getContainer()->get('request_stack')->getCurrentRequest()->attributes->get('pageModel');
+            $objArticle = ArticleModel::findById($arrTag[1]);
+
+            if(!($objPage || $objArticle)){
+                return "";
+            }
+
+            $strUrl = "";
+            if($objPage->id != $objArticle->pid){
+                $objParentPage = PageModel::findById($objArticle->pid);
+                $strUrl = $this->pageResolverService->generate($objParentPage, [], UrlGeneratorInterface::RELATIVE_PATH);
+            }
+
+            $id = 'article-' . $objArticle->id;
+
+            // Generate the CSS ID if it is not set
+            if (empty($cssID = (StringUtil::deserialize($objArticle->cssID)[0])))
+            {
+                $cssID = $id;
+            }
+            return $strUrl.="#$cssID";
+        }
+
+        return false;
+    }
+}
---
+++ src/Picker/AnchorPickerProvider.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Kiwi\Contao\CmxBundle\Picker;
+
+use Contao\CoreBundle\Picker\AbstractInsertTagPickerProvider;
+use Contao\CoreBundle\Picker\DcaPickerProviderInterface;
+use Contao\CoreBundle\Picker\PickerConfig;
+use Knp\Menu\FactoryInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class AnchorPickerProvider extends AbstractInsertTagPickerProvider implements DcaPickerProviderInterface
+{
+    /**
+     * @internal
+     */
+    public function __construct(
+        FactoryInterface $menuFactory,
+        RouterInterface $router,
+        TranslatorInterface $translator,
+        private readonly Security $security,
+    ) {
+        parent::__construct($menuFactory, $router, $translator);
+    }
+
+    public function getName(): string
+    {
+        return 'anchorPicker';
+    }
+
+    public function supportsContext(string $context): bool
+    {
+        return 'link' === $context && $this->security->isGranted('contao_user.modules', 'article');
+    }
+
+    public function supportsValue(PickerConfig $config): bool
+    {
+        return $this->isMatchingInsertTag($config);
+    }
+
+    public function getDcaTable(PickerConfig|null $config = null): string
+    {
+        return 'tl_article';
+    }
+
+    public function getDcaAttributes(PickerConfig $config): array
+    {
+        $attributes = ['fieldType' => 'radio'];
+
+        if ($this->supportsValue($config)) {
+            $attributes['value'] = $this->getInsertTagValue($config);
+
+            if ($flags = $this->getInsertTagFlags($config)) {
+                $attributes['flags'] = $flags;
+            }
+        }
+
+        return $attributes;
+    }
+
+    public function convertDcaValue(PickerConfig $config, mixed $value): int|string
+    {
+        return \sprintf($this->getInsertTag($config), $value);
+    }
+
+    protected function getRouteParameters(PickerConfig|null $config = null): array
+    {
+        return ['do' => 'article'];
+    }
+
+    protected function getDefaultInsertTag(): string
+    {
+        return '{{anchor_url::%s}}';
+    }
+}
