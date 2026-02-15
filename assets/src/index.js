c5dbbcd175dff3a3de5d95607db02da411bd8e29
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-02-15 02:05:17 +0100

    Prefix the choices component

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,7 +1,6 @@
 import "./iconedSelect.scss";
 import "./ui.scss";
 import Choices from "choices.js";
-import "choices.js/public/assets/styles/choices.css";
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.iconedSelect select').forEach(iconedSelect=>{
@@ -21,12 +20,33 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
 
-        const choices = new Choices(iconedSelect, {
+        const cmx_choices = new Choices(iconedSelect, {
             choices: arrOptions,
             allowHTML: true,
             itemSelectText: '',
             shouldSort: false,
             searchFields: ['customProperties.label'],
+            classNames: {
+                containerOuter: ['cmx_choices'],
+                containerInner: ['cmx_choices__inner'],
+                input: ['cmx_choices__input'],
+                inputCloned: ['cmx_choices__input--cloned'],
+                list: ['cmx_choices__list'],
+                listItems: ['cmx_choices__list--multiple'],
+                listSingle: ['cmx_choices__list--single'],
+                listDropdown: ['cmx_choices__list--dropdown'],
+                item: ['cmx_choices__item'],
+                itemSelectable: ['cmx_choices__item--selectable'],
+                itemDisabled: ['cmx_choices__item--disabled'],
+                itemChoice: ['cmx_choices__item--choice'],
+                description: ['cmx_choices__description'],
+                placeholder: ['cmx_choices__placeholder'],
+                group: ['cmx_choices__group'],
+                groupHeading: ['cmx_choices__heading'],
+                button: ['cmx_choices__button'],
+                notice: ['cmx_choices__notice'],
+                addChoice: ['cmx_choices__item--selectable', 'add-choice'],
+            },
         })
     })
 })
