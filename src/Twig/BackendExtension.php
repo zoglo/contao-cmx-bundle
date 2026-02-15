c7d41be1f2508d7c011cb86d678c70cc509c21f4
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-02-15 02:21:10 +0100

    Update the frontend UI twig template

--- src/Twig/BackendExtension.php
+++ src/Twig/BackendExtension.php
@@ -3,42 +3,52 @@
 namespace Kiwi\Contao\CmxBundle\Twig;
 
 use Contao\System;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class BackendExtension extends AbstractExtension
 {
+    public function __construct(
+        private readonly RequestStack $requestStack,
+    ) {
+    }
+
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('csrf', [$this, 'getCSRF']),
             new TwigFunction('clipboard', [$this, 'getClipboard']),
             new TwigFunction('copiedArticle', [$this, 'getCopiedArticleId']),
+            new TwigFunction('csrf', [$this, 'getCSRF']),
             new TwigFunction('isPreview', [$this, 'isPreview']),
         ];
     }
 
+    public function getClipboard()
+    {
+        return $this->requestStack->getSession()->get('CLIPBOARD');
+    }
+
+    public function getCopiedArticleId()
+    {
+        $arrClipboard = $this->requestStack->getSession()->get('CLIPBOARD');
+
+        return ($arrClipboard['tl_article']['mode'] ?? false) == 'copy' ? $arrClipboard['tl_article']['id'] : false;
+    }
+
+    /**
+     * @deprecated Deprecated since version 1.x, to be removed in version 2.x;
+     * *           use contao.request_token instead.
+     */
     public function getCSRF(): string
     {
         return System::getContainer()->get('contao.csrf.token_manager')->getDefaultTokenValue();
     }
 
-    public function getClipboard()
-    {
-        $objSession = System::getContainer()->get('request_stack')->getSession();
-        $arrClipboard = $objSession->get('CLIPBOARD');
-
-        return $arrClipboard;
-    }
-
-    public function getCopiedArticleId()
-    {
-        $objSession = System::getContainer()->get('request_stack')->getSession();
-        $arrClipboard = $objSession->get('CLIPBOARD');
-
-        return ($arrClipboard['tl_article']['mode'] ?? false) == 'copy' ? $arrClipboard['tl_article']['id'] : false;
-    }
-
+    /**
+     * @deprecated Deprecated since version 1.x, to be removed in version 2.x;
+     * *           use contao.is_preview_mode instead.
+     */
     public function isPreview()
     {
         return System::getContainer()->get('request_stack')->getCurrentRequest()->attributes->get('_preview');
